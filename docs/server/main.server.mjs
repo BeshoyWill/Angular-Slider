d3ba7c9de4abdb5fc938c16b04a164f587a42988
Beshoy William <165554296+BeshoyWill@users.noreply.github.com>
2024-10-06 16:18:25 +0300

    'new'

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P4GINUCG.mjs";import"./chunk-ZOPHFMVH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AI7F7W56.mjs";import"./chunk-ZOPHFMVH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
